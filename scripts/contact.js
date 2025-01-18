30e789f329d59c7d0754c1abe66ac92cb0a7badb
Cade Gore <79346293+cadegore@users.noreply.github.com>
2025-01-18 16:37:40 -0500

    remove alert

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,114 +1,114 @@
 // An alert message about this page
-alert(`On this page:
-- You can select one of the location to show the phone number at that location.
-- You can select one of the contact reason to show the different form.
-- You can select one of the contact choice (email or phone number) to show the corresponding input field.
+// alert(`On this page:
+// - You can select one of the location to show the phone number at that location.
+// - You can select one of the contact reason to show the different form.
+// - You can select one of the contact choice (email or phone number) to show the corresponding input field.
 
-- If you select the phone as contact choice, you can enter the phone number in this format: 123-456-7890. Otherwise, It's not valid.
+// - If you select the phone as contact choice, you can enter the phone number in this format: 123-456-7890. Otherwise, It's not valid.
 
-- All input fields will show green border and background when all input are valid, otherwise it will show in red border and red background.
+// - All input fields will show green border and background when all input are valid, otherwise it will show in red border and red background.
 
-- Make sure you submit the form after you finish filling out the form. It will get redirected to another page.
+// - Make sure you submit the form after you finish filling out the form. It will get redirected to another page.
 
-That's all for now! Thank you!
-`);
+// That's all for now! Thank you!
+// `);
 
 // get all the elements
-var hidden = document.querySelectorAll('.hidden');
-var contactReasonEl = document.getElementById('contactReason');
+var hidden = document.querySelectorAll(".hidden");
+var contactReasonEl = document.getElementById("contactReason");
 var userContactChoiceEl = document.querySelectorAll('input[name="userContactChoice"]');
-var userAboutEl = document.getElementById('userAbout');
-var userEmailEl = document.querySelector('.useremail');
-var contactPhoneEl = document.getElementById('contactPhoneNumber');
+var userAboutEl = document.getElementById("userAbout");
+var userEmailEl = document.querySelector(".useremail");
+var contactPhoneEl = document.getElementById("contactPhoneNumber");
 var userJobChoiceEl = document.querySelector('input[name="userJobChoice"]');
-var userEmailInput = document.getElementById('userEmail');
-var userPhoneInput = document.getElementById('userPhone');
-var userMessage = document.getElementById('userMessage');
+var userEmailInput = document.getElementById("userEmail");
+var userPhoneInput = document.getElementById("userPhone");
+var userMessage = document.getElementById("userMessage");
 
 // create event listener if neccessary
-contactReasonEl.addEventListener('change', userContactReaon);
-userContactChoiceEl[0].addEventListener('change', userContactChoice);
-userContactChoiceEl[1].addEventListener('change', userContactChoice);
-contactPhoneEl.addEventListener('change', showPhoneNumber);
+contactReasonEl.addEventListener("change", userContactReaon);
+userContactChoiceEl[0].addEventListener("change", userContactChoice);
+userContactChoiceEl[1].addEventListener("change", userContactChoice);
+contactPhoneEl.addEventListener("change", showPhoneNumber);
 
 // functions
 
 function showPhoneNumber() {
   var contactPhone = contactPhoneEl[contactPhoneEl.selectedIndex].value;
-  if (contactPhone == 'location1') {
-    hidden[0].classList.remove('hidden');
-    hidden[1].classList.add('hidden');
-  } else if (contactPhone == 'location2') {
-    hidden[1].classList.remove('hidden');
-    hidden[0].classList.add('hidden');
+  if (contactPhone == "location1") {
+    hidden[0].classList.remove("hidden");
+    hidden[1].classList.add("hidden");
+  } else if (contactPhone == "location2") {
+    hidden[1].classList.remove("hidden");
+    hidden[0].classList.add("hidden");
   } else {
-    hidden[0].classList.add('hidden');
-    hidden[1].classList.add('hidden');
+    hidden[0].classList.add("hidden");
+    hidden[1].classList.add("hidden");
   }
 }
 
 function phoneNumberCheck() {
-  userPhoneInput.addEventListener('input', () => {
-    userPhoneInput.setCustomValidity('');
+  userPhoneInput.addEventListener("input", () => {
+    userPhoneInput.setCustomValidity("");
     userPhoneInput.checkValidity();
   });
 
-  userPhoneInput.addEventListener('invalid', () => {
-    if (userPhoneInput.value === '') {
-      userPhoneInput.setCustomValidity('Enter phone number!');
+  userPhoneInput.addEventListener("invalid", () => {
+    if (userPhoneInput.value === "") {
+      userPhoneInput.setCustomValidity("Enter phone number!");
     } else {
-      userPhoneInput.setCustomValidity('Enter phone number in this format: 123-456-7890');
+      userPhoneInput.setCustomValidity("Enter phone number in this format: 123-456-7890");
     }
   });
 }
 
 function userContactReaon() {
   var contactReason = contactReasonEl[contactReasonEl.selectedIndex].value;
-  if (contactReason == 'applyforjob') {
-    hidden[2].classList.toggle('hidden');
-    hidden[3].classList.add('hidden');
-    hidden[4].classList.remove('hidden');
-    userJobChoiceEl.setAttribute('required', '');
-    userAboutEl.setAttribute('required', '');
-    userEmailEl.setAttribute('required', '');
-    userMessage.removeAttribute('required');
-    userAboutEl.setAttribute('minlength', '100');
-    userMessage.removeAttribute('minlength');
-  } else if (contactReason == 'others') {
-    hidden[4].classList.remove('hidden');
-    hidden[3].classList.remove('hidden');
-    hidden[2].classList.add('hidden');
-    userMessage.setAttribute('required', '');
-    userJobChoiceEl.removeAttribute('required');
-    userAboutEl.removeAttribute('required');
-    userMessage.setAttribute('minlength', '50');
-    userAboutEl.removeAttribute('minlength');
+  if (contactReason == "applyforjob") {
+    hidden[2].classList.toggle("hidden");
+    hidden[3].classList.add("hidden");
+    hidden[4].classList.remove("hidden");
+    userJobChoiceEl.setAttribute("required", "");
+    userAboutEl.setAttribute("required", "");
+    userEmailEl.setAttribute("required", "");
+    userMessage.removeAttribute("required");
+    userAboutEl.setAttribute("minlength", "100");
+    userMessage.removeAttribute("minlength");
+  } else if (contactReason == "others") {
+    hidden[4].classList.remove("hidden");
+    hidden[3].classList.remove("hidden");
+    hidden[2].classList.add("hidden");
+    userMessage.setAttribute("required", "");
+    userJobChoiceEl.removeAttribute("required");
+    userAboutEl.removeAttribute("required");
+    userMessage.setAttribute("minlength", "50");
+    userAboutEl.removeAttribute("minlength");
   } else {
-    hidden[2].classList.add('hidden');
-    hidden[3].classList.add('hidden');
-    hidden[4].classList.add('hidden');
-    userJobChoiceEl.removeAttribute('required');
-    userAboutEl.removeAttribute('required');
-    userEmailEl.removeAttribute('required');
+    hidden[2].classList.add("hidden");
+    hidden[3].classList.add("hidden");
+    hidden[4].classList.add("hidden");
+    userJobChoiceEl.removeAttribute("required");
+    userAboutEl.removeAttribute("required");
+    userEmailEl.removeAttribute("required");
   }
 }
 
 function userContactChoice() {
   if (userContactChoiceEl[0].checked) {
-    hidden[5].classList.remove('hidden');
-    hidden[6].classList.add('hidden');
-    userPhoneInput.setAttribute('required', '');
-    userEmailInput.removeAttribute('required');
-    userEmailInput.setAttribute('type', 'text');
-    userPhoneInput.setAttribute('pattern', '[0-9]{3}-[0-9]{3}-[0-9]{4}');
+    hidden[5].classList.remove("hidden");
+    hidden[6].classList.add("hidden");
+    userPhoneInput.setAttribute("required", "");
+    userEmailInput.removeAttribute("required");
+    userEmailInput.setAttribute("type", "text");
+    userPhoneInput.setAttribute("pattern", "[0-9]{3}-[0-9]{3}-[0-9]{4}");
     phoneNumberCheck();
   } else if (userContactChoiceEl[1].checked) {
-    userEmailInput.setAttribute('type', 'email');
-    hidden[5].classList.add('hidden');
-    hidden[6].classList.remove('hidden');
-    userEmailInput.setAttribute('required', '');
-    userPhoneInput.removeAttribute('required');
-    userPhoneInput.setAttribute('type', 'text');
-    userPhoneInput.removeAttribute('pattern');
+    userEmailInput.setAttribute("type", "email");
+    hidden[5].classList.add("hidden");
+    hidden[6].classList.remove("hidden");
+    userEmailInput.setAttribute("required", "");
+    userPhoneInput.removeAttribute("required");
+    userPhoneInput.setAttribute("type", "text");
+    userPhoneInput.removeAttribute("pattern");
   }
 }
--- scripts/order.js
+++ scripts/order.js
@@ -1,24 +1,24 @@
 // An alert to tell about this page
-alert(`On this page:
-- You can click or type in the quantity of each dish to get the total cost.
-- If you order more than 10 for each dish, the input will have red border and background.
-- The total cost will be calculated automatically (in real time) when the quantity and tip increase or decrease.
+// alert(`On this page:
+// - You can click or type in the quantity of each dish to get the total cost.
+// - If you order more than 10 for each dish, the input will have red border and background.
+// - The total cost will be calculated automatically (in real time) when the quantity and tip increase or decrease.
 
-JUST INCASE:
-- If you enter a negative number, the total cost will show the cost in negative but will put negative sign in front of dollar sign.
+// JUST INCASE:
+// - If you enter a negative number, the total cost will show the cost in negative but will put negative sign in front of dollar sign.
 
-Later:
-- A comfirmation page will be created soon in the future.
+// Later:
+// - A comfirmation page will be created soon in the future.
 
-Thank you!
-`);
+// Thank you!
+// `);
 
 // get all the HTML elements that are needed
-var dishPrices = document.querySelectorAll('.dish_price span');
+var dishPrices = document.querySelectorAll(".dish_price span");
 var dishPrice = [];
-var taxCost = document.getElementById('tax_cost');
-var totalCost = document.getElementById('total_cost');
-var tip = document.getElementById('tippercent');
+var taxCost = document.getElementById("tax_cost");
+var totalCost = document.getElementById("total_cost");
+var tip = document.getElementById("tippercent");
 
 // Pass the dish price into an array for later use
 Array.from(dishPrices).forEach(function (price) {
@@ -38,9 +38,9 @@ var subtotalCost = {
 };
 
 // function to calculate the tip that is selected
-tip.addEventListener('change', tipTaxCalculation);
+tip.addEventListener("change", tipTaxCalculation);
 function tipTaxCalculation(value) {
-  var tipEl = document.getElementById('tip_total');
+  var tipEl = document.getElementById("tip_total");
   var subtotal = Object.values(subtotalCost)
     .reduce((a, b) => a + b)
     .toFixed(2);
@@ -68,7 +68,7 @@ function tipTaxCalculation(value) {
 
 // function to calculate the total cost of entire order
 function orderTotal() {
-  var subtotalEl = document.getElementById('subtotal');
+  var subtotalEl = document.getElementById("subtotal");
   var subtotal = Object.values(subtotalCost)
     .reduce((a, b) => a + b)
     .toFixed(2);
@@ -81,107 +81,107 @@ function orderTotal() {
 }
 
 // Douhua Total Cost
-var douhuaQuantity = document.getElementById('douhua_order_quantity');
-douhuaQuantity.addEventListener('change', douhuaTotal);
+var douhuaQuantity = document.getElementById("douhua_order_quantity");
+douhuaQuantity.addEventListener("change", douhuaTotal);
 
 function douhuaTotal(value) {
   value = Number(douhuaQuantity.value);
   douhuaTotalCost = Math.round(value * Number(dishPrice[0]) * 100) / 100;
   if (douhuaTotalCost < 0) {
-    document.getElementById('douhua_total').innerHTML = `-$${Math.abs(douhuaTotalCost).toFixed(2)}`;
+    document.getElementById("douhua_total").innerHTML = `-$${Math.abs(douhuaTotalCost).toFixed(2)}`;
   } else {
-    document.getElementById('douhua_total').innerHTML = `$${douhuaTotalCost.toFixed(2)}`;
+    document.getElementById("douhua_total").innerHTML = `$${douhuaTotalCost.toFixed(2)}`;
   }
   subtotalCost.douhua = douhuaTotalCost;
   orderTotal();
 }
 
 // Youtiao Total Cost
-var youtiaoQuantity = document.getElementById('youtiao_order_quantity');
-youtiaoQuantity.addEventListener('change', youtiaoTotal);
+var youtiaoQuantity = document.getElementById("youtiao_order_quantity");
+youtiaoQuantity.addEventListener("change", youtiaoTotal);
 
 function youtiaoTotal(value) {
   value = Number(youtiaoQuantity.value);
   youtiaoTotalCost = Math.round(value * Number(dishPrice[1]) * 100) / 100;
   if (youtiaoTotalCost < 0) {
-    document.getElementById('youtiao_total').innerHTML = `-$${Math.abs(youtiaoTotalCost).toFixed(
+    document.getElementById("youtiao_total").innerHTML = `-$${Math.abs(youtiaoTotalCost).toFixed(
       2
     )}`;
   } else {
-    document.getElementById('youtiao_total').innerHTML = `$${youtiaoTotalCost.toFixed(2)}`;
+    document.getElementById("youtiao_total").innerHTML = `$${youtiaoTotalCost.toFixed(2)}`;
   }
   subtotalCost.youtiao = youtiaoTotalCost;
   orderTotal();
 }
 
 // Steamed Fish Total Cost
-var steamedFishQuantity = document.getElementById('steamed_fish_order_quantity');
+var steamedFishQuantity = document.getElementById("steamed_fish_order_quantity");
 
-steamedFishQuantity.addEventListener('change', steamedFishTotal);
+steamedFishQuantity.addEventListener("change", steamedFishTotal);
 
 function steamedFishTotal(value) {
   value = Number(steamedFishQuantity.value);
   steamedFishTotalCost = Math.round(value * Number(dishPrice[2]) * 100) / 100;
   if (steamedFishTotalCost < 0) {
-    document.getElementById('steamed_fish_total').innerHTML = `-$${Math.abs(
+    document.getElementById("steamed_fish_total").innerHTML = `-$${Math.abs(
       steamedFishTotalCost
     ).toFixed(2)}`;
   } else {
-    document.getElementById('steamed_fish_total').innerHTML = `$${steamedFishTotalCost.toFixed(2)}`;
+    document.getElementById("steamed_fish_total").innerHTML = `$${steamedFishTotalCost.toFixed(2)}`;
   }
   subtotalCost.steamedFish = steamedFishTotalCost;
   orderTotal();
 }
 
 // Green Beans Total Cost
-var greenBeansQuantity = document.getElementById('green_beans_order_quantity');
-greenBeansQuantity.addEventListener('change', greenBeansTotal);
+var greenBeansQuantity = document.getElementById("green_beans_order_quantity");
+greenBeansQuantity.addEventListener("change", greenBeansTotal);
 
 function greenBeansTotal(value) {
   value = Number(greenBeansQuantity.value);
   greenBeansTotalCost = Math.round(value * Number(dishPrice[3]) * 100) / 100;
   if (greenBeansTotalCost < 0) {
-    document.getElementById('green_beans_total').innerHTML = `-$${Math.abs(
+    document.getElementById("green_beans_total").innerHTML = `-$${Math.abs(
       greenBeansTotalCost
     ).toFixed(2)}`;
   } else {
-    document.getElementById('green_beans_total').innerHTML = `$${greenBeansTotalCost.toFixed(2)}`;
+    document.getElementById("green_beans_total").innerHTML = `$${greenBeansTotalCost.toFixed(2)}`;
   }
   subtotalCost.greenBeans = greenBeansTotalCost;
   orderTotal();
 }
 
 // Beef Ramen Total Cost
-var beefRamenQuantity = document.getElementById('beef_ramen_order_quantity');
-beefRamenQuantity.addEventListener('change', beefRamenTotal);
+var beefRamenQuantity = document.getElementById("beef_ramen_order_quantity");
+beefRamenQuantity.addEventListener("change", beefRamenTotal);
 
 function beefRamenTotal(value) {
   value = Number(beefRamenQuantity.value);
   beefRamenTotalCost = Math.round(value * Number(dishPrice[4] * 100)) / 100;
   if (beefRamenTotalCost < 0) {
-    document.getElementById('beef_ramen_total').innerHTML = `-$${Math.abs(
+    document.getElementById("beef_ramen_total").innerHTML = `-$${Math.abs(
       beefRamenTotalCost
     ).toFixed(2)}`;
   } else {
-    document.getElementById('beef_ramen_total').innerHTML = `$${beefRamenTotalCost.toFixed(2)}`;
+    document.getElementById("beef_ramen_total").innerHTML = `$${beefRamenTotalCost.toFixed(2)}`;
   }
   subtotalCost.beefRamen = beefRamenTotalCost;
   orderTotal();
 }
 
 // Egg Fried Rice Total Cost
-var eggFriedRiceQuantity = document.getElementById('egg_fried_rice_order_quantity');
-eggFriedRiceQuantity.addEventListener('change', eggFriedRiceTotal);
+var eggFriedRiceQuantity = document.getElementById("egg_fried_rice_order_quantity");
+eggFriedRiceQuantity.addEventListener("change", eggFriedRiceTotal);
 
 function eggFriedRiceTotal(value) {
   value = Number(eggFriedRiceQuantity.value);
   eggFriedRiceTotalCost = Math.round(value * Number(dishPrice[5]) * 100) / 100;
   if (eggFriedRiceTotalCost < 0) {
-    document.getElementById('egg_fried_rice_total').innerHTML = `-$${Math.abs(
+    document.getElementById("egg_fried_rice_total").innerHTML = `-$${Math.abs(
       eggFriedRiceTotalCost
     ).toFixed(2)}`;
   } else {
-    document.getElementById('egg_fried_rice_total').innerHTML = `$${eggFriedRiceTotalCost.toFixed(
+    document.getElementById("egg_fried_rice_total").innerHTML = `$${eggFriedRiceTotalCost.toFixed(
       2
     )}`;
   }
@@ -190,19 +190,19 @@ function eggFriedRiceTotal(value) {
 }
 
 // Steamed Dumplings Total Cost
-var steamedDumplingsQuantity = document.getElementById('steamed_dumplings_order_quantity');
-steamedDumplingsQuantity.addEventListener('change', steamedDumplingsTotal);
+var steamedDumplingsQuantity = document.getElementById("steamed_dumplings_order_quantity");
+steamedDumplingsQuantity.addEventListener("change", steamedDumplingsTotal);
 
 function steamedDumplingsTotal(value) {
   value = Number(steamedDumplingsQuantity.value);
   steamedDumplingsTotalCost = Math.round(value * Number(dishPrice[6]) * 100) / 100;
   if (steamedDumplingsTotalCost < 0) {
-    document.getElementById('steamed_dumplings_total').innerHTML = `-$${Math.abs(
+    document.getElementById("steamed_dumplings_total").innerHTML = `-$${Math.abs(
       steamedDumplingsTotalCost
     ).toFixed(2)}`;
   } else {
     document.getElementById(
-      'steamed_dumplings_total'
+      "steamed_dumplings_total"
     ).innerHTML = `$${steamedDumplingsTotalCost.toFixed(2)}`;
   }
   subtotalCost.steamedDumplings = steamedDumplingsTotalCost;
@@ -210,18 +210,18 @@ function steamedDumplingsTotal(value) {
 }
 
 // Century Egg Total Cost
-var centuryEggQuantity = document.getElementById('century_egg_order_quantity');
-centuryEggQuantity.addEventListener('change', centuryEggTotal);
+var centuryEggQuantity = document.getElementById("century_egg_order_quantity");
+centuryEggQuantity.addEventListener("change", centuryEggTotal);
 
 function centuryEggTotal(value) {
   value = Number(centuryEggQuantity.value);
   centuryEggTotalCost = Math.round(value * Number(dishPrice[7]) * 100) / 100;
   if (centuryEggTotalCost < 0) {
-    document.getElementById('century_egg_total').innerHTML = `-$${Math.abs(
+    document.getElementById("century_egg_total").innerHTML = `-$${Math.abs(
       centuryEggTotalCost
     ).toFixed(2)}`;
   } else {
-    document.getElementById('century_egg_total').innerHTML = `$${centuryEggTotalCost.toFixed(2)}`;
+    document.getElementById("century_egg_total").innerHTML = `$${centuryEggTotalCost.toFixed(2)}`;
   }
   subtotalCost.centuryEgg = centuryEggTotalCost;
   orderTotal();
